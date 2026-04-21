9d11adf69375eb05efb731c66c27dc36ea319c09
udochrist <udo.christ@me.com>
2026-04-21 18:14:11 +0200

    Refactor error handling for process startup

--- owfs-app.sh
+++ owfs-app.sh
@@ -4,20 +4,18 @@
 echo "Starting owserver"
 
 /usr/bin/owserver -c /etc/owfs.conf
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start 'owserver': $status"
-  exit $status
+if [ $? -ne 0 ]; then
+  echo "Failed to start owserver"
+  exit 1
 fi
 
 # Start the second process: owhttpd
 echo "Starting owhttpd"
 
 /usr/bin/owhttpd -c /etc/owfs.conf
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start 'owhttpd': $status"
-  exit $status
+if [ $? -ne 0 ]; then
+  echo "Failed to start owhttpd"
+  exit 1
 fi
 
 # keep running forever
